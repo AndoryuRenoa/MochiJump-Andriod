3d84b106223315cf8c52e3794e00c1864ede5fd9
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-09-02 18:48:21 -0600

    update

--- MochiJumpAndroid/app/src/main/java/com/mochijump/mochijumpandroid/StartScreen.java
+++ MochiJumpAndroid/app/src/main/java/com/mochijump/mochijumpandroid/StartScreen.java
@@ -1,13 +1,27 @@
 package com.mochijump.mochijumpandroid;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StartScreen extends AppCompatActivity {
+ImageButton start = (ImageButton) findViewById(R.id.startButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                System.out.println("test Accepted");
+
+            }
+        });
     }
+
+
+
 }
