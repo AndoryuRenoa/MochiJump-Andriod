81ee2c80f283f85edd7714599ab235e68909d473
AndoryuRenoa <35511163+AndoryuRenoa@users.noreply.github.com>
2018-09-03 14:03:19 -0600

    learned resizing

--- MochiJumpAndroid/app/src/main/java/com/mochijump/mochijumpandroid/StartScreen.java
+++ MochiJumpAndroid/app/src/main/java/com/mochijump/mochijumpandroid/StartScreen.java
@@ -1,20 +1,30 @@
 package com.mochijump.mochijumpandroid;
 
-import android.app.Dialog;
+
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class StartScreen extends AppCompatActivity {
-ImageButton start = (ImageButton) findViewById(R.id.startButton);
+ImageButton start;
+ImageView mochiIcon;
 
+Display display;
+Point size = new Point();
+int screenWidth;
+int screenHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-// okay if I understand this right, I want to start here and treat this as the main() method. I can then build my framework
-// around this. Odd that andriod studio just sticks all this into one class.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        start =  (ImageButton) findViewById(R.id.startButton);
+        mochiIcon = (ImageView) findViewById(R.id.mochiIcon);
+        display= getWindowManager().getDefaultDisplay();
+        resizeScreen();
         start.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -23,4 +33,16 @@ ImageButton start = (ImageButton) findViewById(R.id.startButton);
             }
         });
     }
+
+    public void resizeScreen(){
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        mochiIcon.getLayoutParams().width= (222*screenWidth/1366);
+        mochiIcon.getLayoutParams().height = (225*screenHeight/786);
+    }
+
+
+
 }
